e87bfb1c746cf8c4b467a8ea5852f9da127d8278
oanaradulescu <oana.a.radulescu@gmail.com>
2014-05-13 19:36:04 -0400

    new file

---
+++ complete.R
@@ -0,0 +1,16 @@
+complete <- function(directory, id = 1:332) {
+    
+    monitordata<-NULL
+    df<-NULL
+    
+    for (i in id) {
+        df<-read.csv(paste(directory,"/",sprintf("%03s",i),".csv",sep=""))
+        monitordata<-rbind(monitordata,df)
+    }
+    
+    good.bv<-complete.cases(monitordata)
+    good.df<-monitordata[good.bv,]
+    cnts<-tapply(good.df[,4],as.factor(good.df[,4]),length,simplify=F)
+    
+    cnts.df<-as.data.frame(cnts)
+}
