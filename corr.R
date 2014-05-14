4b863d8a64d39e1925b5e5049ffe621e5a8867aa
oanaradulescu <oana.a.radulescu@gmail.com>
2014-05-13 23:36:39 -0400

    latest

--- corr.R
+++ corr.R
@@ -10,15 +10,18 @@ corr <- function(directory, threshold = 0) {
         
         ## Return a numeric vector of correlations
         
-        complete.counts <- complete(directory)	
-        counts.above <- complete.counts["nobs"] > threshold
-        file.ids <- complete.counts["id"][counts.above]
+        complete.counts <- complete(directory)        
+        counts.above <- complete.counts[,"nobs"] > threshold
+        file.ids <- complete.counts[counts.above,"id"]
         
-        result <- vector()
+        result <- vector(mode="numeric",length=length(file.ids))
+        
+        count<-0
         
         for (id in file.ids) {
                 monitor.obs <- read.csv(paste(directory, "/", sprintf("%03s", as.character(id)), ".csv", sep=""))
-                result <- c(result, cor(monitor.obs["nitrate"], monitor.obs["sulfate"], use="complete.obs"))
+                count <- count+1
+                result[count] <- cor(monitor.obs["nitrate"], monitor.obs["sulfate"], use="complete.obs")
         }
         
         result
